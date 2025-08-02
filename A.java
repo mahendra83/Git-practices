0dc540274f53fdaec71971c3295138bcd9967308
mahendra83 <mahe33648@gmail.com>
2025-08-02 23:28:56 +0530

    d

--- A.java
+++ A.java
@@ -1,12 +1,11 @@
 class{
 	
-	System.out.println("hi");
-	
-	
-	
-	
-	
+	System.out.println("hi");	
+		
 	public void m3(){
 		System.out.print("data received");
+
+	void m1(){
+		// logic needs to be implemented
 	}
 }
