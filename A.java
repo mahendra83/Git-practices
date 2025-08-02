cee1170b63df4f4294b491985f8695523136cf4c
mahendra83 <mahe33648@gmail.com>
2025-08-02 23:44:21 +0530

    Revert "d"
    This reverts commit 0dc540274f53fdaec71971c3295138bcd9967308.
    
    just checking

--- A.java
+++ A.java
@@ -1,11 +1,12 @@
 class{
 	
-	System.out.println("hi");	
-		
+	System.out.println("hi");
+	
+	
+	
+	
+	
 	public void m3(){
 		System.out.print("data received");
-
-	void m1(){
-		// logic needs to be implemented
 	}
 }
